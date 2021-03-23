bc21f836b5e64bb2601529b7e4ebfde96bf043f2
Mark Wells <maw461@psu.edu>
2021-03-23 12:33:07 -0400

    fix integrate dispatch
    changed "function integrate(x, y...) end" to "function integrate end"

--- src/NumericalIntegration.jl
+++ src/NumericalIntegration.jl
@@ -33,7 +33,7 @@ const HALF = 1//2
 
 Compute numerical integral of y(x) from x=x[1] to x=x[end]. Return a scalar of the same type as the input. If no method is supplied, use Trapezdoial.
 """
-function integrate(x,y...) end
+function integrate end
 
 
 """
